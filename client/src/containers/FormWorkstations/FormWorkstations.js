6a8f51ed2c53b9f457247e266e4810b005a535fa
xJaguar25x <22175098+xJaguar25x@users.noreply.github.com>
2021-05-30 19:24:15 +0300

    Added validation of the ip-address, name, and type fields.

--- client/src/containers/FormWorkstations/FormWorkstations.js
+++ client/src/containers/FormWorkstations/FormWorkstations.js
@@ -10,6 +10,7 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -45,20 +46,42 @@ export default function FormWorkstations() {
         fetchData();
     }, []);
 
-   /* const [age, setAge] = React.useState('Pc');
-
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };*/
+    const validateName = (value) => {
+        let error;
+        if (!value) {
+            error = 'Required';
+        }
+        return error;
+    };
+    const validateType = (value) => {
+        let error;
+        if (!value) {
+            error = 'Required';
+        }
+        return error;
+    };
+    const validateIpAddress = (value) => {
+        let error;
+        if (!value) {
+            error = 'Required';
+        } else if (!/^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$/i.test(value)) {
+            error = 'Invalid ip address';
+        } else if (workstations.some(n => {
+            return n.ip_address === value
+        })) {
+            error = 'This ip already exists! Ip address must be unique';
+        }
+        return error;
+    };
 
     const onSubmit = async (values) => {
-        console.log("submit: ", values);
-        console.log(values);
+        // console.log("submit: ", values);
+        // console.log(values);
 
         try {
             const res = await axios.post('api/workstations', values);
             setMessage({status: 200, msg: 'Save successful'});
-            console.log("upload data: ", res);
+            // console.log("upload data: ", res);
         } catch (err) {
             if (err.response.status === 500) {
                 setMessage({status: '500', msg: 'There was a problem with a server'});
@@ -75,50 +98,65 @@ export default function FormWorkstations() {
             onSubmit(values)
         }}
       >
-          {(formProps) => (
+          {({errors, touched, handleChange, values, isValid, isValidating}) => (
             <Form className={classes.root}>
                 {message ? <Message msg={message}/> : null}
-                <Field name="name"
-                       id="form-name"
-                       label="name"
-                       value={formProps.values.name}
-                       as={TextField}
-                />
-                <FormControl className={classes.formControl}>
+                <FormControl required className={classes.formControl}>
+                    <Field name="name"
+                           id="form-name"
+                           label="name"
+                           value={values.name}
+                           required
+                           validate={validateName}
+                           error={errors["name"]}
+                           as={TextField}
+                    />
+                    <FormHelperText>{errors.name && touched.name && errors.name}</FormHelperText>
+                </FormControl>
+                <FormControl required className={classes.formControl}>
                     <InputLabel
                       htmlFor="form-type"
-                      //id="form-type-label"
+                      // id="form-type-label"
                     >type</InputLabel>
                     <Field name="type"
                            id="form-type"
-                           //labelId="form-type-label"
+                      // labelId="form-type-label"
                            label="type"
-                           value={formProps.values.type}
-                           // defaultValue={formProps.values.type}//если нужно значение по умолчанию
+                           value={values.type}
+                      // defaultValue={values.type}//если нужно значение по умолчанию
+                           validate={validateType}
+                           error={errors["type"]}
                            as={Select}>
                         <MenuItem value="">None</MenuItem>
                         <MenuItem value="pc">Pc</MenuItem>
                         <MenuItem value="server">Server</MenuItem>
                         <MenuItem value="boards">Boards</MenuItem>
                     </Field>
+                    <FormHelperText>{errors.type && touched.type && errors.type}</FormHelperText>
                 </FormControl>
-                <Field name="ip_address"
-                       id="form-type-ip_address"
-                       label="ip_address"
-                       value={formProps.values.ip_address}
-                       as={TextField}
-                />
+                <FormControl required className={classes.formControl}>
+                    <Field name="ip_address"
+                           id="form-type-ip_address"
+                           label="ip_address"
+                           value={values.ip_address}
+                           as={TextField}
+                           required
+                           validate={validateIpAddress}
+                           error={errors["ip_address"]}
+                    />
+                    <FormHelperText>{errors.ip_address && touched.ip_address && errors.ip_address}</FormHelperText>
+                </FormControl>
                 <Field name="location"
                        id="form-type-location"
                        label="location"
-                       value={formProps.values.location}
+                       value={values.location}
                        multiline
                        as={TextField}
                 />
                 <Field name="description"
                        id="form-type-description"
                        label="description"
-                       value={formProps.values.description}
+                       value={values.description}
                        multiline
                        as={TextField}
                 />
@@ -133,7 +171,9 @@ export default function FormWorkstations() {
                     Save
                 </Button>
                 {/*для отладки*/}
-                {/*<pre>{JSON.stringify(formProps.values, null, 2)}</pre>*/}
+                {/* <pre>"values" :{JSON.stringify(values, null, 2)}</pre>
+                <pre>"errors" :{JSON.stringify(errors, null, 2)}</pre>
+                <pre>"touched" :{JSON.stringify(touched, null, 2)}</pre>*/}
             </Form>
           )}
       </Formik>
